000e2d9ae9a19a57e54440eefdd3db9b240b7a3e
Erick Ramos <erick.ramos@ericsson.com>
2021-01-26 17:06:31 -0600

    primer dia

---
+++ strings.rb
@@ -0,0 +1,8 @@
+a = "Esto es un lindo string"
+b = "otro string"
+#en Ruby casi todo es un objeto
+puts a.length
+puts b.length
+#String de multiples lineas
+c = "Esto es \ntring\nmultiples lineas"
+puts c
---
+++ variables.rb
@@ -0,0 +1,12 @@
+#ruby puede tener numeros enteros
+puts "Entero "
+a = 37
+a + 1
+puts a
+a = a + 1
+puts a
+#ruby puede tener decimales
+b = 2.5
+c = 1 
+puts "Decimales"
+puts b + c #el resultado de un entero mas un decimal es decimal
